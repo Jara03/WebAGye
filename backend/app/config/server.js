904ced06978bac9a4929e0284c7d8d77f884a9aa
Iliès BENYAHIA <ilies.benyahia@free.fr>
2021-08-19 11:27:20 +0200

    Premier push backend

---
+++ backend/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f63b6710e4a5ba21f7c81421da378012'),
+    },
+  },
+});
---
+++ backend/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '620b5881-d000-4ab6-90d9-35bf88bb1f5f'
+};
